e74493b107b0154a3d67a95213a51ce0ca52c904
Ella Belfer <e.belfer@berkeley.edu>
2022-07-18 17:40:02 -0400

    Outlining workflow. In progress

--- test_platform/scripts/2_clean_data/MARITIME_clean.py
+++ test_platform/scripts/2_clean_data/MARITIME_clean.py
@@ -1,16 +1,50 @@
+# Import library
+import os
+import xarray as xr
+from datetime import datetime
+
 # Read in downloaded data and do first clean.
 ## Code written first by file, then rewrite to include merging mult. files (by time or by station?)
 
-# Step one: read in, and drop variables that are not of interest.
-filename = "32302_198901.nc"
+# Get current time
+timestamp = datetime.now()
+
+# Set envr vars.
+workdir = "/home/ella/Desktop/Eagle-Rock/Historical-Data-Platform/Maritime/"
 file = os.path.join(workdir, filename) 
 
 # Create list of variables to remove
 dropvars = ['wave_wpm', 'wave_wpm_bnds', 'bottom_depth', 'magnetic_variation','sampling_rates_waves', 'sampling_duration_waves', 'total_intervals_waves', 'significant_wave_height', 'average_wave_period', 'dominant_wave_period','spectral_density_c']
 
+# Only keep stations in WECC
+
+# For each station, join files together (by year or by all time?).
+filename = "32302_198901.nc"
+files = os.listdir(workdir)
+files = list(filter(lambda f: f.endswith('.nc'), files))
+stations = 
+#for stationname in stations:
+#    ds = xr.open_mfdataset('{}_*.nc'.format(stationname), combine = 'by_coord', concat_dim = 'time')
+
 # Read in file and drop variables that aren't of interest.
 ds = xr.open_dataset(file, drop_variables = dropvars)
-print(ds)
+
+# Convert date-time to CF-compliant format (datetime object, UTC).
+## Data is CF1.6 compliant, time in datetime format.
+
+# Convert station metadata to standard format.
+## One column for lat (y), one column for lon (x).
+
+# Add dataset metadata in standard format.
+
+# Convert missing data to common format.
+### Use NaN.
+
+
+# Convert existing QA/QC flags to standard format.
+
+# Deduplicate / merge files?
+print(ds.attrs)
 
 # Step two: 
 # In the long run use this code to open and merge multiple files (by station? by year?)
