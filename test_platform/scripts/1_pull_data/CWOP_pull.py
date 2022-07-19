1e7e646b57ae09ef131607cea026d2f6200d4351
Ella Belfer <e.belfer@berkeley.edu>
2022-07-19 16:17:51 -0400

    Compiling links for CWOP pull from MADIS

---
+++ test_platform/scripts/1_pull_data/CWOP_pull.py
@@ -0,0 +1,3 @@
+# Use ftp
+# Each directory is year / month / day / LDAD / mesonet / netcdf 
+### We probably want to use this method to filter by bbox and provider: https://madis-data.ncep.noaa.gov/madis_ldad_filter.shtml
