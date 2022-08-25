fca478d19cc54a203ac87988d0c3864197c91df4
Victoria Ford <fordvecellio@gmail.com>
2022-08-25 17:13:44 -0400

    adds _get_wecc_poly to calc_clean

--- test_platform/scripts/2_clean_data/calc_clean.py
+++ test_platform/scripts/2_clean_data/calc_clean.py
@@ -11,6 +11,19 @@ import numpy as np
 import geopandas as gp
 from math import exp, log
 
+## Useful functions
+def get_wecc_poly(terrpath, marpath):
+    """
+    Identifies a bbox of WECC area to filter stations against
+    Input vars: shapefiles for maritime and terrestrial WECC boundaries
+    Returns: spatial objects for each shapefile, and bounding box for their union.
+    """
+    t = gp.read_file(terrpath)  ## Read in terrestrial WECC shapefile.
+    m = gp.read_file(marpath)   ## Read in marine WECC shapefile.
+    bbox = t.union(m).bounds    ## Combine polygons and get bounding box of union.
+    return t,m, bbox
+
+
 ## Unit conversions, if required
 ## Temperature conversions: Desired working unit should be K
 def _unit_degC_to_K(data):
