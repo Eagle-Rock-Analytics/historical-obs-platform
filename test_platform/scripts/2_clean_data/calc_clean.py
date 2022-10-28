291a618e4e641733f6f2579449a32e1f80a933ed
Victoria Ford <fordvecellio@gmail.com>
2022-10-28 18:13:14 -0400

    indent off on lat function

--- test_platform/scripts/2_clean_data/calc_clean.py
+++ test_platform/scripts/2_clean_data/calc_clean.py
@@ -151,10 +151,10 @@ def _lon_DMm_to_Dd(data):
     Input: longitude (DDDMM.mm) example: 12234.72
     Returns: longitude (D.d) example: -122.578
     """
-     _min = float(data[:3])
-     _sec = float(data[3:])
-     data = -1 * (_deg + _mm/60)
-     return data
+    _min = float(data[:3])
+    _sec = float(data[3:])
+    data = -1 * (_deg + _mm/60)
+    return data
 
 def _lat_DMm_to_Dd(data):
     """
