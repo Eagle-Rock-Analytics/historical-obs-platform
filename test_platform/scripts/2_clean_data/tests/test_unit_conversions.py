5fe19f1cb104122e77e8da329df2cc55a7287649
Victoria Ford <fordvecellio@gmail.com>
2022-11-02 16:36:47 -0400

    all fns working except lat/lon -- in testing

--- test_platform/scripts/2_clean_data/tests/test_unit_conversions.py
+++ test_platform/scripts/2_clean_data/tests/test_unit_conversions.py
@@ -1,5 +1,7 @@
 """
-This script runs tests that unit conversions perform as expected
+This script runs tests that unit conversions perform as expected.
+
+To run: navigate to the folder where this is stored, and run "pytest test_unit_conversions.py" or just "pytest" from the command line
 """
 import pandas as pd
 import pytest
@@ -11,21 +13,25 @@ from calc_clean import (_unit_degC_to_K,
                         _unit_pres_hpa_to_pa,
                         _unit_pres_inHg_to_pa,
                         _unit_elev_ft_to_m,
+                        _unit_moisture_gkg_to_kgkg,
                         _lat_dms_to_dd,
                         _lat_DMm_to_Dd,
                         _lon_dms_to_dd,
                         _lon_DMm_to_Dd)
 
-test_data = pd.read_csv("../tests/test_dataset.csv")
+@pytest.fixture
+def df():
+    return pd.read_csv('test_dataset.csv')
 
 ## -----------------------------------------------------------------------------------------
 ## Temperature conversions -- desired unit is K
 @pytest.fixture
-def grab_temp_C(test_data):
-    return test_data['temp_c']
+def grab_temp_C(df):
+    return df['temp_c']
 
-def grab_temp_f(test_data):
-    return test_data['temp_f']
+@pytest.fixture
+def grab_temp_f(df):
+    return df['temp_f']
 
 def test_temp_conversion_degC(grab_temp_C):
     """Test that the _unit_degC_to_K function correctly converts from degC to K"""
@@ -45,9 +51,9 @@ def test_temp_conversion_degF(grab_temp_f):
 ## Note: Scaling to hourly data will occur in qa/qc step
 ## Note: Therefore this unit test for the cleaning stage will ensure that data is in mm per unit of time at present pre scaling
 @pytest.fixture
-def grab_precip(test_data):
+def grab_precip(df):
     """Grab the precipitation variable. Native units could be mm or inches"""
-    return test_data['precip_in']
+    return df['precip_in']
 
 def test_precip_conversion_inches(grab_precip):
     """Test that the _unit_precip_in_to_mm function correctly converts from inches to mm"""
@@ -59,12 +65,13 @@ def test_precip_conversion_inches(grab_precip):
 ## -----------------------------------------------------------------------------------------
 ## Wind conversions -- desired unit is m/s
 @pytest.fixture
-def grab_wind_kts(test_data):
+def grab_wind_kts(df):
     """Grab the wind variable. Native units could be m/s, mph, knots"""
-    return test_data['wind_kts']
+    return df['wind_kts']
 
-def grab_wind_mph(test_data):
-    return test_data['wind_mph']
+@pytest.fixture
+def grab_wind_mph(df):
+    return df['wind_mph']
 
 def test_wind_conversion_kts(grab_wind_kts):
     """Test that the _unit_windspd_kts_to_ms correctly converts from knots to m/s"""
@@ -82,12 +89,13 @@ def test_wind_conversion_mph(grab_wind_mph):
 ## -----------------------------------------------------------------------------------------
 ## Pressure conversions -- desired unit is Pascal
 @pytest.fixture
-def grab_pressure_hpa(test_data):
+def grab_pressure_hpa(df):
     """Grab the pressure variable. Native units could be hPa, mb, Pa, inHg"""
-    return test_data['press_hpa']
+    return df['press_hpa']
 
-def grab_pressure_inhg(test_data):
-    return test_data['press_inhg']
+@pytest.fixture
+def grab_pressure_inhg(df):
+    return df['press_inhg']
 
 def test_pressure_conversion_hpa(grab_pressure_hpa):
     """Test that the _unit_pres_hpa_to_pa correctly converts from hPa to Pa"""
@@ -106,9 +114,9 @@ def test_pressure_conversion_inHg(grab_pressure_inhg):
 ## Moisture conversions -- desired unit is kg/kg
 ## Note: Not a primary variable, but optionally needed for some derivations
 @pytest.fixture
-def grab_moisture(test_data):
+def grab_moisture(df):
     """Grabs the water vapor mixing ratio. Native units could be g/kg or kg/kg"""
-    return test_data['moisture_kgkg']
+    return df['moisture_gkg']
 
 def test_moisture_conversion_kgkg(grab_moisture):
     """Test that the _unit_moisture_gkg_to_kgkg correctly converts from g/kg to kg/kg"""
@@ -120,9 +128,9 @@ def test_moisture_conversion_kgkg(grab_moisture):
 ## -----------------------------------------------------------------------------------------
 ## Elevation conversions -- desired unit is m
 @pytest.fixture
-def grab_elevation(test_data):
+def grab_elevation(df):
     """Grabs the elevation. Native units could be m or feet"""
-    return test_data['elev_feet'] ## check variable name
+    return df['elev_feet']
 
 def test_elevation_conversion_feet(grab_elevation):
     """Test that the _unit_elev_ft_to_m correctly converts from feet to m"""
@@ -134,35 +142,44 @@ def test_elevation_conversion_feet(grab_elevation):
 ## -----------------------------------------------------------------------------------------
 ## Latitude conversions -- desired unit is decimal degrees
 @pytest.fixture
-def grab_latitude_dms(test_data):
+def grab_latitude_dms(df):
     """Grabs the latitude. Native units could be decimal-minutes-seconds (DMS), decimal degrees (D.d), or LORAN (DM.m)"""
-    return test_data['lat_dms'] ## check variable name
+    return df['lat_dms'] ## check variable name
 
-def grab_latitude_dmm(test_data):
-    return test_data['lat_dmm']
+@pytest.fixture
+def grab_latitude_dmm(df):
+    return df['lat_dmm']
 
 def test_latitude_conversion_dms(grab_latitude_dms):
     """Test that the _lat_dms_to_dd correctly converts from DMS to D.d"""
     calc_clean_converted = _lat_dms_to_dd(grab_latitude_dms)
-    correct_conversion = float(grab_latitude_dms[:2]) + float(grab_latitude_dms[3:5])/60 + float(grab_latitude_dms[6:])/3600
-    assert correct_conversion.equals(calc_clean_converted)
+
+    for i in range(len(grab_latitude_dms)):
+        _deg = float(grab_latitude_dms[i][:2])
+        _min = float(grab_latitude_dms[i][3:5])
+        _sec = float(grab_latitude_dms[i][6:])
+    correct_conversion = _deg + _min/60 + _sec/3600
+    assert correct_conversion == calc_clean_converted
 
 def test_latitude_conversion_dmm(grab_latitude_dmm):
     """Test that the _lat_DMm_to_Dd correctly converts from DMm to D.d"""
     calc_clean_converted = _lat_DMm_to_Dd(grab_latitude_dmm)
-    correct_conversion = float(grab_latitude_dmm[:2]) + float(grab_latitude_dmm[2:])/60
+    _lat = grab_latitude_dmm['lat_dmm'][:2].apply(lambda x: float(x))
+    _min = grab_latitude_dmm['lat_dmm'][2:].apply(lambda y: float(y))
+    correct_conversion = _lat + _min/60
     assert correct_conversion.equals(calc_clean_converted)
 
 
 ## -----------------------------------------------------------------------------------------
 ## Longitude conversions -- desired unit is decimal degrees
 @pytest.fixture
-def grab_longitude_dms(test_data):
+def grab_longitude_dms(df):
     """Grabs the longitude. Native units could be decimal-minutes-seconds (DMS), decimal degrees (D.d), or LORAN (DM.m)"""
-    return test_data['lon_dms'] ## check variable name
+    return df['lon_dms'] ## check variable name
 
-def grab_longitude_dmm(test_data):
-    return test_data['lon_dmm']
+@pytest.fixture
+def grab_longitude_dmm(df):
+    return df['lon_dmm']
 
 def test_longitude_conversion_dms(grab_longitude_dms):
     """Test that the _lon_dms_to_dd correctly converts from DMS to D.d"""
