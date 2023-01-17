d7f436a4cc494f229d4499014ebda81a62057500
Victoria Ford <fordvecellio@gmail.com>
2023-01-17 17:53:36 -0500

    add get_wecc_poly to calc_qaqc

--- test_platform/scripts/3_qaqc_data/calc_qaqc.py
+++ test_platform/scripts/3_qaqc_data/calc_qaqc.py
@@ -10,6 +10,7 @@ import pandas as pd
 import xarray as xr
 import boto3
 import s3fs
+import geopandas as gp
 
 
 ## Set AWS credentials
@@ -24,13 +25,25 @@ wecc_mar = "s3://wecc-historical-wx/0_maps/WECC_Informational_MarineCoastal_Boun
 
 #----------------------------------------------------------------------
 ## QA/QC Helper functions
-# Given a network name, return all relevant AWS filepaths for other functions
 def get_file_paths(network):
     rawdir = "1_raw_wx/{}/".format(network)
     cleandir = "2_clean_wx/{}/".format(network)
     qaqcdir = "3_qaqc_wx/{}/".format(network)
     mergedir = "4_merge_wx/{}/".format(network)
     return rawdir, cleandir, qaqcdir, mergedir
+
+
+def get_wecc_poly(terrpath, marpath):
+    """
+    Identifies a bbox of WECC area to filter stations against
+    Input vars: shapefiles for maritime and terrestrial WECC boundaries
+    Returns: spatial objects for each shapefile, and bounding box for their union.
+    """
+    t = gp.read_file(terrpath)  ## Read in terrestrial WECC shapefile.
+    m = gp.read_file(marpath)   ## Read in marine WECC shapefile.
+    bbox = t.union(m).bounds    ## Combine polygons and get bounding box of union.
+    return t,m, bbox
+
 
 #----------------------------------------------------------------------
 ## Part 1 functions (whole station/network)
@@ -39,44 +52,10 @@ def qaqc_missing_latlon(df):
     """
     Checks if latitude and longitude is missing for a station.
     If missing, station is flagged to not proceed through QA/QC.
-    Args:
-        df: dataframe of cleaned station data to check
-    Returns:
-
     """
-    # first check if latitude is present in the dataframe
-    if 'lat' in df.columns:
-        if df['lat'].isnull().values.any() == True:
-            print('missing latitude, skipping')
-            # errors['File'].append(file)
-            # errors['Time'].append(end_api)
-            # errors['Error'].append('Missing latitude, skipping qa/qc.')
-            # continue
-    else:
-        print('df has no latitude coordinate')
-
-    # check if longitude is present in the dataframe
-    if 'lon' in df.columns:
-        if df['lon'].isnull().values.any() == True:
-            print('missing longitude, skipping')
-            # errors['File'].append(file)
-            # errors['Time'].append(end_api)
-            # errors['Error'].append('Missing longitude, skipping qa/qc.')
-            # continue
-    else:
-        print('df has no longitude coordinate')
-
-    return df
-
-
-
-
-
-
-
 
 ## in bounds of WECC
-def qaqc_within_wecc():
+def qaqc_within_wecc(df):
     """
     Checks if station is within terrestrial & marine WECC boundaries.
     If outside of boundaries, station is flagged to not proceed through QA/QC.
@@ -92,10 +71,7 @@ def qaqc_elev_check(df):
     # Step 1: identify range of reasonable values
     # Step 2: DEM
     # Step 3: missing elevation check
-    if df['elevation'].isnull().values.any() == True:
-        print('missing elevation, will be processed via DEM')
-    else:
-        print('all valid elevation values')
+
 
 #----------------------------------------------------------------------
 ## Part 2 functions
