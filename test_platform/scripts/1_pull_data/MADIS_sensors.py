a8fefa9e6d4fdaf54c13ff256ca2d04dce70a558
Victoria Ford <fordvecellio@gmail.com>
2023-01-25 15:50:52 -0500

    fix spacing issue in CAHYDRO name

--- test_platform/scripts/1_pull_data/MADIS_sensors.py
+++ test_platform/scripts/1_pull_data/MADIS_sensors.py
@@ -1,3 +1,10 @@
+"""
+This script outputs a sensor list csv for each of the MADIS networks and is saved to the
+qaqc_wx folder for each network.
+
+This script only needs to be run occasionally/when a full MADIS pull occurs.
+"""
+
 # Step 0: Environment set-up
 import requests
 import pandas as pd
@@ -27,7 +34,7 @@ wecc_mar = "s3://wecc-historical-wx/0_maps/WECC_Informational_MarineCoastal_Boun
 directory = "3_qaqc_wx/"
 
 # List of MADIS network names
-madis_networks = ['CAHYDRO', 'CDEC', 'CNRFC', 'CRN', 'CWOP', 'HNXWFO', 'HOLFUY', 'HPWREN', 'LOXWFO', 'MAP', 'MTRWFO', 'NCAWOS', 'NOS-NWLON', 'NOS-PORTS',
+madis_networks = ['CA HYDRO', 'CDEC', 'CNRFC', 'CRN', 'CWOP', 'HNXWFO', 'HOLFUY', 'HPWREN', 'LOXWFO', 'MAP', 'MTRWFO', 'NCAWOS', 'NOS-NWLON', 'NOS-PORTS',
 'RAWS', 'SGXWFO', 'SHASAVAL', 'VCAPCD']
 
 # Get dictionary of network names and short IDs
@@ -40,6 +47,7 @@ def madis_network_name_to_id(token, networks):
     networks = networks[networks['SHORTNAME'].isin(shortnames)]
     networks = networks[['SHORTNAME', 'ID']]
     networks['SHORTNAME'] = networks['SHORTNAME'].replace(to_replace="APRSWXNET/CWOP", value = "CWOP")
+    networks['SHORTNAME'] = networks['SHORTNAME'].replace(to_replace="CA HYDRO", value="CAHYDRO") # removes space issue
     return networks
 
 
@@ -52,17 +60,17 @@ def get_madis_sensor_metadata(token, terrpath, marpath, networks, bucket_name, d
         for index, row in networks.iterrows():
             networkname = row['SHORTNAME']
             networkid = row['ID']
-            
+
             # Access station metadata to get list of IDs in bbox and network
             # Using: https://developers.synopticdata.com/mesonet/v2/stations/timeseries/
             url = "https://api.synopticdata.com/v2/stations/metadata?token={}&network={}&bbox={}&complete=1&sensorvars=1&recent=20&output=json".format(token, networkid, bbox_api)
             request = requests.get(url).json()
-            
+
             ids = []
             station_list = pd.DataFrame(request['STATION'])
             station_list = pd.concat([station_list, station_list["PERIOD_OF_RECORD"].apply(pd.Series)], axis=1) # Split Period of Record column
             station_list = pd.concat([station_list, station_list["UNITS"].apply(pd.Series)], axis=1) # Split Period of Record column
-            station_list = pd.concat([station_list, station_list["SENSOR_VARIABLES"].apply(pd.Series)], axis=1) # Split Variables column
+            station_list = pd.concat([station_list, station_list["SENSOR_VARIABLES"].apply(pd.Series, dtype='string')], axis=1) # Split Variables column
             station_list = station_list.drop("PERIOD_OF_RECORD", axis =1)
             station_list = station_list.drop("UNITS", axis =1)
             station_list = station_list.drop("SENSOR_VARIABLES", axis =1)
@@ -75,10 +83,10 @@ def get_madis_sensor_metadata(token, terrpath, marpath, networks, bucket_name, d
 
             # Get index of 'end', last standard column
             end_ind = station_list.columns.get_loc("end")
-            
+
             # Drop columns
             station_list = station_list.drop(columns=[col for col in station_list.columns[end_ind+1:] if col not in coltokeep]) # Drop all columns not in coltokeep list.
-        
+
             # Each sensor has a column of dictionaries. For each sensor, collapse column.
             cols_in_df = [i for i in coltokeep if i in station_list.columns]
             for i in cols_in_df:
@@ -96,13 +104,14 @@ def get_madis_sensor_metadata(token, terrpath, marpath, networks, bucket_name, d
 
             # Save station list to AWS
             savedir = directory+networkname+"/"
-            
+
             csv_buffer_err = StringIO()
             station_list.to_csv(csv_buffer_err)
             content = csv_buffer_err.getvalue()
-            
+
+            print("Sensor list saved for {}.".format(networkname))
             s3_cl.put_object(Bucket=bucket_name, Body=content, Key=savedir+"sensorlist_{}.csv".format(networkname))
-        
+
     except Exception as e:
         print("Error: {}".format(e))
 
