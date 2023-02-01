388c6224b29d473d1658b0c6ac27f082893482ae
Victoria Ford <fordvecellio@gmail.com>
2023-02-01 11:22:14 -0500

    fix bad merge conflict

--- test_platform/scripts/2_clean_data/MADIS_clean.py
+++ test_platform/scripts/2_clean_data/MADIS_clean.py
@@ -261,11 +261,7 @@ def clean_madis(bucket_name, rawdir, cleandir, network):
         sensor_data = pd.read_csv(smart_open.smart_open(sensor_filepath))
 
     except Exception as e: # If unable to read files from cleandir, break function.
-<<<<<<< HEAD
-        print("whole network error")
-=======
         print("Whole network error.")
->>>>>>> 1b3d8f3481f853298302463739d61f8c427b792e
         errors['File'].append("Whole network")
         errors['Time'].append(end_api)
         errors['Error'].append("Whole network error: {}".format(e))
