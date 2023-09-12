8033c66dc445fda2ec9c15cc8d10e89d750510dc
indahector <indahector@gmail.com>
2023-09-11 18:19:01 -0600

    argument parsing added at ALLNETWORKS_qaqc.py

--- test_platform/scripts/3_qaqc_data/ALLNETWORKS_qaqc.py
+++ test_platform/scripts/3_qaqc_data/ALLNETWORKS_qaqc.py
@@ -314,8 +314,26 @@ def whole_station_qaqc(network, cleandir, qaqcdir):
 ## -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Run function
 if __name__ == "__main__":
-
-    network = "RAWS"
+    
+    # Create parser
+    parser = argparse.ArgumentParser(
+        prog="ALLNETWORKS_qaqc",
+        description="""This script performs qa/qc protocols for cleaned station data for ingestion into the Historical 
+                       observations Platform, and is independent of network.""",
+        epilog="""Possible stations:
+                  [ASOSAWOS, CAHYDRO, CIMIS, CW3E, CDEC, CNRFC, CRN, CWOP, HADS, HNXWFO, 
+                   HOLFUY, HPWREN, LOXWFOMAP, MTRWFO, NCAWOS, NOS-NWLON, NOS-PORTS, OtherISD, 
+                   RAWS, SGXWFO, SHASAVAL, VCAPCD, MARITIME, NDBC, SCAN, SNOTEL]
+               """
+    )
+    
+    # Define arguments for the program
+    parser.add_argument('-n', '--network', default="VCAPCD")
+    parser.add_argument('-v', '--verbose', default="True")
+    
+    # Parse arguments
+    args = parser.parse_args()
+    network = args.network
 
     rawdir, cleandir, qaqcdir, mergedir = get_file_paths(network)
     whole_station_qaqc(network, cleandir, qaqcdir)
@@ -325,3 +343,4 @@ if __name__ == "__main__":
 # output csv of flags/consistent flagging
 # check the h5netcdf vs. netcdf4 engine
 # delete testing notes
+
