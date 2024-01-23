a91c38422a68b0e9559f2f9fe8749ca371cd73a8
indahector <indahector@gmail.com>
2024-01-23 15:14:09 -0600

    Default Verbose to False

--- test_platform/scripts/3_qaqc_data/ALLNETWORKS_qaqc.py
+++ test_platform/scripts/3_qaqc_data/ALLNETWORKS_qaqc.py
@@ -53,10 +53,10 @@ if __name__ == "__main__":
     )
     
     # Define arguments for the program
-    parser.add_argument('-n', '--network', default="VCAPCD", help="Network name (Default=VCAPCD)", type=str)
-    parser.add_argument('-l', '--local', default=False, help="Save files and plots locally (Default=False)", type=bool)
-    parser.add_argument('-r', '--rad_scheme', default="remove_zeros", help="Radiation handling scheme for frequent values check. See qaqc_frequent_values for options (Default='remove_zeros'", type=str)
-    parser.add_argument('-v', '--verbose', default=False, help="Print statements throughout script (Default=False)", type=bool)
+    parser.add_argument('-n', '--network', default="VCAPCD", help="Network name (default to 'VCAPCD')", type=str)
+    parser.add_argument('-l', '--local', default=False, help="Save files and plots locally (default to False)", type=bool)
+    parser.add_argument('-r', '--rad_scheme', default="remove_zeros", help="Radiation handling scheme for frequent values check. See qaqc_frequent_values for options (default to 'remove_zeros'", type=str)
+    parser.add_argument('-v', '--verbose', default=False, help="Print statements throughout script (default to False)", type=bool)
     
     # Parse arguments
     args = parser.parse_args()
