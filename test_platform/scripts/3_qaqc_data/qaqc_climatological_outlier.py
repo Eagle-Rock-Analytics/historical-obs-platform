02a8e33b0eba2f12902871a5292f79267d0d01fe
Victoria Ford <fordvecellio@gmail.com>
2024-02-27 12:16:19 -0500

    update clim

--- test_platform/scripts/3_qaqc_data/qaqc_climatological_outlier.py
+++ test_platform/scripts/3_qaqc_data/qaqc_climatological_outlier.py
@@ -128,7 +128,7 @@ def qaqc_climatological_outlier(df, winsorize=True, winz_limits=[0.05,0.05], plo
                                 for i in idx_to_flag:
                                     df.loc[df.index == i, var+'_eraqc'] = 26 # see era_qaqc_flag_meanings.csv 
 
-            if plot == True:
+            if plot:
                 for var in vars_to_anom:
                     for month in range(1,13):
                         if 26 in df[var+'_eraqc'].values: # only plot a figure if flag is present
