457eb1a861861a74cdf6ac9b27cb78ef5c1b5838
Victoria Ford <fordvecellio@gmail.com>
2025-01-24 11:32:23 -0500

    im an idiot

--- test_platform/scripts/3_qaqc_data/qaqc_logic_checks.py
+++ test_platform/scripts/3_qaqc_data/qaqc_logic_checks.py
@@ -61,13 +61,17 @@ def qaqc_crossvar_logic_tdps_to_tas_supersat(df, verbose=False):
                 # only use valid obs for both dewpoint and air temp
                 df_valid = grab_valid_obs(df, var="tas", var2=dew_var)
                 isBad = df_valid.loc[df_valid[dew_var] > df_valid["tas"]]
-                df.loc[isBad.index, dew_var + "_eraqc"] = 12 # see qaqc_flag_meanings.csv
+                df.loc[
+                    isBad.index, dew_var + "_eraqc"
+                ] = 12  # see qaqc_flag_meanings.csv
 
         return df
 
     except Exception as e:
         logger.info(
-            "qaqc_crossvar_logic_tdps_to_tas_supersat failed with Exception: {}".format(e)
+            "qaqc_crossvar_logic_tdps_to_tas_supersat failed with Exception: {}".format(
+                e
+            )
         )
         return None
 
@@ -147,7 +151,9 @@ def qaqc_crossvar_logic_tdps_to_tas_wetbulb(df, verbose=False):
 
     except Exception as e:
         logger.info(
-            "qaqc_crossvar_logic_tdps_to_tas_wetbulb failed with Exception: {}".format(e)
+            "qaqc_crossvar_logic_tdps_to_tas_wetbulb failed with Exception: {}".format(
+                e
+            )
         )
         return None
 
