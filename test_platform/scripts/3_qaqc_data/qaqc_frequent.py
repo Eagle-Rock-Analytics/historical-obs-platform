bb3d62495e906a55de2a8bf9e964f8ded443fbc5
Victoria Ford <fordvecellio@gmail.com>
2025-01-30 13:05:21 -0600

    rm awkward testing print statement

--- test_platform/scripts/3_qaqc_data/qaqc_frequent.py
+++ test_platform/scripts/3_qaqc_data/qaqc_frequent.py
@@ -201,7 +201,6 @@ def qaqc_frequent_vals(df, rad_scheme, plots=True, verbose=False, local=False):
     try:
         # precip focused check
         for var in pr_vars_to_check:
-            print("hi", var)
             df = qaqc_frequent_precip(df, var)
 
     except Exception as e:
@@ -274,14 +273,6 @@ def frequent_bincheck(df, var, data_group, rad_scheme, verbose=False):
                 "Radiation frequent value check scheme: remove_zeros selected, may remove valid daytime (cloudy) conditions",
             )
             df_to_test = df.loc[df[var] >= get_bin_size_by_var(var)]
-
-    # # Don't check for zeros in precip vars
-    # # We expect a lot of the precip data to be zero and don't want to flag frequent zeros
-    # elif var in ["pr_5min", "pr_15min", "pr_1h", "pr_24h", "pr_localmid"]:
-    #     logger.info(
-    #         "Precipitation frequent value check scheme: QAQC will not flag high frequency of zeroes, because high frequency of zero precipitation is expected",
-    #     )
-    #     df_to_test = df.loc[df[var] >= get_bin_size_by_var(var)]
 
     else:  # all other variables
         df_to_test = df
