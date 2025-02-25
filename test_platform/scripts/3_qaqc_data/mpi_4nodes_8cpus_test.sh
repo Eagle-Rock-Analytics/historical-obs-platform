531299f5bf2494cd04fdc2d4e36ebf0f67f24fd7
Nicole Keeney <nicolejkeeney@gmail.com>
2025-02-25 21:38:11 +0000

    this one works;

--- test_platform/scripts/3_qaqc_data/mpi_4nodes_8cpus_test.sh
+++ test_platform/scripts/3_qaqc_data/mpi_4nodes_8cpus_test.sh
@@ -10,30 +10,27 @@
 # Python Script: /shared/nicole/historical-obs-platform/test_platform/scripts/3_qaqc_data/ALLNETWORKS_qaqc.py
 # Arguments: network="CAHYDRO"
 
-# Set output and error file paths
-OUTPUT_FILE="${HOME}/%x_%j_output.txt"
-ERROR_FILE="${HOME}/%x_%j_error.txt"
-
 #SBATCH --job-name=hist-obs          # Name of the job
 #SBATCH --ntasks=8                   # Total number of MPI processes (1 per CPU)
 #SBATCH --nodes=4                    # Number of nodes (1 node with 2 CPUs per node)
 #SBATCH --ntasks-per-node=2          # Number of MPI processes per node (2 processes per node)
 #SBATCH --time=5:00:00               # Maximum runtime (adjust as necessary)
 #SBATCH --partition=test             # Queue/partition (adjust as necessary)
-#SBATCH --output=$OUTPUT_FILE        # Standard output file with job name and job ID
-#SBATCH --error=$ERROR_FILE          # Standard error file with job name and job ID
+#SBATCH --output=%x_%j_output.txt    # Standard output file with job name and job ID
+#SBATCH --error=%x_%j_error.txt      # Standard error file with job name and job ID
+
+# AWS secret info 
+export AWS_ACCESS_KEY_ID="put-your-key-id-here"
+export AWS_SECRET_ACCESS_KEY="put-your-key-here"
+export AWS_DEFAULT_REGION="us-west-2"
 
 # Load required modules (OpenMPI and Conda)
 module load openmpi
 
 # Define the path to your Python script
-PYSCRIPT="${HOME}/historical-obs-platform/test_platform/scripts/3_qaqc_data/ALLNETWORKS_qaqc.py"
-
-# Check the current working directory
-echo "Current working directory: $(pwd)"
+PYSCRIPT="ALLNETWORKS_qaqc.py"
 
 # Check if the Python script exists
-echo "Checking if Python script exists: $PYSCRIPT"
 if [ ! -f "$PYSCRIPT" ]; then
   echo "Error: Python script not found!"
   exit 1
@@ -46,7 +43,7 @@ source /shared/miniconda3/etc/profile.d/conda.sh
 conda activate /shared/miniconda3/envs/hist-obs
 
 # Run the Python script directly using Python from the activated environment
-srun --mpi=pmix_v5 python3 ${PYSCRIPT} network="CAHYDRO"
+srun --mpi=pmi2 python3 ${PYSCRIPT} --network="CAHYDRO" --sample=1
 
 # Deactivate Conda environment after job completion
 conda deactivate
