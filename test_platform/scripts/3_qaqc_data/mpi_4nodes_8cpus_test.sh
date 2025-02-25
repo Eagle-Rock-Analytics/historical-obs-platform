770048d46e00e842f4d9f68e32ff466c856b62dc
Nicole Keeney <nicolejkeeney@gmail.com>
2025-02-25 12:42:32 -0700

    troubleshooting

--- test_platform/scripts/3_qaqc_data/mpi_4nodes_8cpus_test.sh
+++ test_platform/scripts/3_qaqc_data/mpi_4nodes_8cpus_test.sh
@@ -8,27 +8,28 @@
 # Python Script: /shared/nicole/historical-obs-platform/test_platform/scripts/3_qaqc_data/ALLNETWORKS_qaqc.py
 # Arguments: network="CAHYDRO"
 
-#SBATCH --job-name=hist-obs          # Name of the job
-#SBATCH --ntasks=8                   # Total number of MPI processes (1 per CPU)
-#SBATCH --nodes=4                    # Number of nodes (1 node with 2 CPUs per node)
-#SBATCH --ntasks-per-node=2          # Number of MPI processes per node (2 processes per node)
-#SBATCH --time=5:00:00               # Maximum runtime (adjust as necessary)
-#SBATCH --partition=test             # Queue/partition (adjust as necessary)
-#SBATCH --output=mpi_job_output2.txt    # Standard output file with job name and job ID
-#SBATCH --error=mpi_job_error2.txt      # Standard error file with job name and job ID
+#SBATCH --job-name=hist-obs                # Name of the job
+#SBATCH --ntasks=8                         # Total number of MPI processes (1 per CPU)
+#SBATCH --nodes=4                          # Number of nodes (1 node with 2 CPUs per node)
+#SBATCH --ntasks-per-node=2                # Number of MPI processes per node (2 processes per node)
+#SBATCH --time=5:00:00                     # Maximum runtime (adjust as necessary)
+#SBATCH --partition=test                   # Queue/partition (adjust as necessary)
+#SBATCH --output=$HOME/%x_%j_output.txt    # Standard output file with job name and job ID
+#SBATCH --error=$HOME/%x_%j_error.txt      # Standard error file with job name and job ID
 
 # Load required modules (OpenMPI and Conda)
 module load openmpi
 
-# Activate conda env 
-conda activate hist-obs
+# Initialize Conda
+source $HOME/miniconda3/etc/profile.d/conda.sh 
+
+# Activate Conda environment (specify full path to the env)
+conda activate /shared/miniconda3/envs/hist-obs
 
 # Define the path to your Python script
-# Make sure this is in relation to your home directory 
-# You can find your home directory with the command echo $HOME
 PYSCRIPT=$HOME/historical-obs-platform/test_platform/scripts/3_qaqc_data/ALLNETWORKS_qaqc.py
 
-# Run the Python script using Conda environment using MPI plugin version 5 
+# Run the Python script using Conda environment with MPI plugin version 5 
 srun --mpi=pmix_v5 conda run -n hist-obs python3 ${PYSCRIPT} network="CAHYDRO" sample=1
 
 # Deactivate Conda environment after job completion
