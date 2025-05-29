09911b199418fdd3bcd9b334761ec28ed90dc063
Nicole Keeney <nicolejkeeney@gmail.com>
2025-05-29 13:22:25 -0600

    add logger

--- test_platform/scripts/4_merge_data/merge_eraqc_counts_original_timestep.py
+++ test_platform/scripts/4_merge_data/merge_eraqc_counts_original_timestep.py
@@ -7,10 +7,12 @@ These tables are used to produce QAQC flag statistics for the QAQC success repor
 """
 
 import pandas as pd
+import logging
+import inspect
 
 
 def eraqc_counts_original_timestep(
-    df: pd.DataFrame, network: str, station: str
+    df: pd.DataFrame, network: str, station: str, logger: logging.Logger
 ) -> None:
     """
     Generates a dataframe of raw qaqc flag value counts for every variable,
@@ -30,27 +32,36 @@ def eraqc_counts_original_timestep(
     -------
     None
     """
-    # identify _eraqc variables
-    eraqc_vars = [var for var in df.columns if "_eraqc" in var]
+    logger.info(f"{inspect.currentframe().f_code.co_name}: Starting...")
 
-    # filter df for only qaqc columns
-    # also replace Nan values with 'no_flag' for two reasons:
-    #   1. to enable us to count total observations for the success report
-    #   2. to clarify what the Nan value indicates
-    df = df[eraqc_vars].fillna("no_flag")
+    try:
+        # identify _eraqc variables
+        eraqc_vars = [var for var in df.columns if "_eraqc" in var]
 
-    # generate df of counts of each unique flag for each variable
-    # fill all Nan values with 0, since Nan = no observations counted
-    flag_counts = df.apply(pd.Series.value_counts).fillna(0)
+        # filter df for only qaqc columns
+        # also replace Nan values with 'no_flag' for two reasons:
+        #   1. to enable us to count total observations for the success report
+        #   2. to clarify what the Nan value indicates
+        df = df[eraqc_vars].fillna("no_flag")
 
-    # rename columns
-    flag_counts.columns = flag_counts.columns.str.replace("_eraqc", "", regex=True)
+        # generate df of counts of each unique flag for each variable
+        # fill all Nan values with 0, since Nan = no observations counted
+        flag_counts = df.apply(pd.Series.value_counts).fillna(0)
 
-    # rename index (i.e. eraqc values) and then reset index
-    flag_counts = flag_counts.rename_axis("eraqc_flag_values").reset_index()
+        # rename columns
+        flag_counts.columns = flag_counts.columns.str.replace("_eraqc", "", regex=True)
 
-    # send file to AWS
-    csv_s3_filepath = f"s3://wecc-historical-wx/4_merge_wx/{network}/eraqc_counts/{station}_flag_counts_native_timestep.csv"
-    flag_counts.to_csv(csv_s3_filepath, index=False)
+        # rename index (i.e. eraqc values) and then reset index
+        flag_counts = flag_counts.rename_axis("eraqc_flag_values").reset_index()
 
-    return None
+        # send file to AWS
+        csv_s3_filepath = f"s3://wecc-historical-wx/4_merge_wx/{network}/eraqc_counts/{station}_flag_counts_native_timestep.csv"
+        flag_counts.to_csv(csv_s3_filepath, index=False)
+
+        # Update logger :)
+        logger.info(f"Uploaded file to: {csv_s3_filepath}")
+        logger.info(f"{inspect.currentframe().f_code.co_name}: Completed successfully")
+
+    except Exception as e:
+        logger.error(f"{inspect.currentframe().f_code.co_name}: Failed")
+        raise e
