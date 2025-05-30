2fa2fbff9bb1b9121b684a76d4b9030757a9fe40
name <vanessa.machuca@eaglerockanalytics.com>
2025-05-30 12:18:22 -0700

    how should we handle data in time gaps for the success report stats? included them in total obs count? if not, need to mark them somehow, no?

--- test_platform/scripts/4_merge_data/merge_eraqc_counts.py
+++ test_platform/scripts/4_merge_data/merge_eraqc_counts.py
@@ -46,14 +46,19 @@ def eraqc_counts_original_timestep(
     # fill all Nan values with 0, since Nan = no observations counted
     flag_counts = df.apply(pd.Series.value_counts).fillna(0)
 
+    # set all counts to integers, for readability
+    flag_counts = flag_counts.astype(int)
+
     # rename columns
     flag_counts.columns = flag_counts.columns.str.replace("_eraqc", "", regex=True)
 
     # rename index (i.e. eraqc values) and then reset index
-    flag_counts = flag_counts.rename_axis("eraqc_flag_values").reset_index()
+    flag_counts = flag_counts.rename_axis("eraqc_flag_values")
 
     # send file to AWS
     csv_s3_filepath = f"s3://wecc-historical-wx/4_merge_wx/{network}/eraqc_counts/{station}_flag_counts_native_timestep.csv"
-    flag_counts.to_csv(csv_s3_filepath, index=False)
+    flag_counts.to_csv(csv_s3_filepath, index=True)
 
     return None
+
+# -----------------------------------------------------------------------------
