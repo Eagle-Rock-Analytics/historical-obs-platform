cf93ce026f2f1199ac289b995d1b0087961f5224
name <vanessa.machuca@eaglerockanalytics.com>
2025-06-27 11:39:03 -0700

    scoping

---
+++ test_platform/scripts/figures/figure_utils.py
@@ -0,0 +1,9 @@
+# 1_pull_stationvisualizer.ipynb
+
+# 2_clean_cleaned_variable_coverage.ipynb
+
+# 2_clean_stationvisualizer_subset.ipynb
+
+# 2_clean_stationvisualizer.ipynb
+
+# 3_qaqc_training_stationvisualizer.ipynb
