a4478534602d94b73a32e73ec9c54fc70484bb5a
name <vanessa.machuca@eaglerockanalytics.com>
2025-08-20 09:28:34 -0700

    started script to house function, also still need to add in try/catch

---
+++ data-access/asos_lookup.py
@@ -0,0 +1,19 @@
+"""
+asos_lookup.py
+
+A user can pass either the ASOS airport code or the “city” and the script/function identifes and returns the name of the HDP station.
+E.g., “I need to find KSAC weather station” or “I need to find the Sacramento Executive Airport weather station”
+
+Script then identifies KSAC as “ASOSAWOS_XXXXXXXX”
+
+Reasoning: Looking up the specific HDP stations from the attached spreadsheet was not straightforward or easy; users may very likely 
+come to HDP knowing specifically which station they are looking for, and it’s not obvious which station is which.
+
+Usage
+-----
+
+
+Output
+------
+
+"""
